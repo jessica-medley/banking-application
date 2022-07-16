28aa7a1deb93a307bcb56b24d5f0388a84ba31c5
jessica-medley <jessicaposey3@gmail.com>
2022-07-16 10:38:30 -0700

    Changing alt tag

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ export default function Home() {
       txtcolor="black"
       header="Welcome to Incredibly Bad Bank"
       text="We will start you off with a $100 balance! Make deposits and withdraw as you need! Feel free to check out the users created during your session in our All Data option in the navigation bar."
-      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
+      body={<img src="bank.png" className="img-fluid" alt="A bank" />}
     />
   );
 }
